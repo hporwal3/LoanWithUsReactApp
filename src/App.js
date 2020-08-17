d2b95c8d76409d3f8164b0b1f4561eb9174bdb68
hporwal3 <porwalhimanshu1997@gmail.com>
2020-08-17 12:23:27 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Loan with Us
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Test
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
